d0ed558cc6745ac4862307eaac84ca423c667513
Katya Akilina <katyaartline@gmail.com>
2022-02-21 01:03:02 +0300

    feat: реализовать функции открытия и закрытия блока popup

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+const popup = document.querySelector('.popup');
+const popupElement = popup.querySelector('.popup__container');
+const popupButtonCloseElement = popupElement.querySelector('.popup__button-close');
+const profile = document.querySelector('.profile');
+const profileButtonEditElement = profile.querySelector('.profile__edit-button');
+
+console.log(popup);
+console.log(popupElement);
+console.log(popupButtonCloseElement);
+
+const togglePopupVisibility = function() {
+    popup.classList.toggle('popup_is-opened');
+}
+
+const openPopup = function() {
+    popup.classList.add('popup_is-opened');
+}
+
+const closePopup = function() {
+    popup.classList.remove('popup_is-opened');
+}
+
+profileButtonEditElement.addEventListener('click', openPopup);
+
+popupButtonCloseElement.addEventListener('click', closePopup);
+
+
