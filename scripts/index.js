940ed9011da948d01c38754383e2bf0272a48460
Katya Akilina <katyaartline@gmail.com>
2022-02-26 12:35:48 +0300

    fix: исправить ошибки в позиционировании popup по центру экрана, исправить размеры profile__info

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,26 @@
 const popup = document.querySelector('.popup');
 const popupElement = popup.querySelector('.popup__container');
 const popupButtonCloseElement = popupElement.querySelector('.popup__button-close');
-const popupButtonSaveElement = popupElement.querySelector('.popup__button-save');
+
 let profile = document.querySelector('.profile');
 let profileInfo = profile.querySelector('.profile__info')
 const profileButtonEditElement = profile.querySelector('.profile__edit-button'); 
 
 let formElement = popupElement.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__inputsitem_type_name');
-let jobInput = formElement.querySelector('.popup__inputsitem_type_description');
+let nameInput = formElement.querySelector('.popup__inputs-item_type_name');
+let jobInput = formElement.querySelector('.popup__inputs-item_type_description');
 
-
-
-const togglePopupVisibility = function() {
-    popup.classList.toggle('popup_opened');
-}
+// Выберите элементы, куда должны быть вставлены значения полей
+let nameProfileValue = profileInfo.querySelector('.profile__name');
+let jobProfileValue = profileInfo.querySelector('.profile__description');
 
 const openPopup = function() {
     popup.classList.add('popup_opened');
+    // Получите значение полей jobInput и nameInput из свойства value
+    let nameInputValue = nameInput.value;
+    let jobInputValue = jobInput.value;
+    nameInputValue = nameProfileValue.textContent;
+    jobInputValue = jobProfileValue.textContent;
 }
 
 const closePopup = function() {
@@ -42,10 +45,6 @@ function formSubmitHandler (event) {
     // Получите значение полей jobInput и nameInput из свойства value
     let nameInputValue = nameInput.value;
     let jobInputValue = jobInput.value;
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let nameProfileValue = profileInfo.querySelector('.profile__name');
-    let jobProfileValue = profileInfo.querySelector('.profile__description');
 
     // Вставьте новые значения с помощью textContent
     nameProfileValue.textContent = nameInputValue;
