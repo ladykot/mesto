7949ecb4625784adb43b4d56b4b60e97731e4b5a
Katya Akilina <katyaartline@gmail.com>
2022-03-26 20:08:48 +0300

    feat: add validation for popup in profile

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,21 +1,66 @@
 const obj = {
     formElement: '.popup__form',
     inputElement: '.popup__inputs-item',
+    // inputSet: '.popup__inputs',
     buttonElement: '.popup__button-save',
-    errorElement: `.${inputElement.id}-error`,
     errorClass: 'popup__inputs-error_active',
     buttonDisableClass: 'popup__button-save_disable',
 }
 
+function getErrorElement(inputElement) {
+    return formElement.querySelector(`.${inputElement.id}-error`);
+}
+
+function showInputError(formElement, inputElement, errorMessage) {
+    errorElement = getErrorElement(inputElement);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('popup__inputs-error_active')
+    inputElement.classList.add('popup__inputs-item_invalid');
+}
+
+function hideInputError(formElement, inputElement) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    errorElement.textContent = '';
+    errorElement.classList.remove('popup__inputs-error_active');
+    inputElement.classList.remove('popup__inputs-item_invalid');
+}
+
+// проверка поля на валидность
+function checkInputValidity(formElement, inputElement) {
+    console.log(inputElement.validity)
+    if(!inputElement.validity.valid) {
+        const errorMessage = inputElement.validationMessage;
+        showInputError(formElement, inputElement, errorMessage);
+    } else {
+        hideInputError(formElement, inputElement);
+    }
+}
+
+// * наложение обработчиков на поля формы
+function setEventListeners(formElement, inputElement) {
+    const inputList = Array.from(formElement.querySelectorAll(inputElement))
+    inputList.forEach(inputItem => {
+        inputItem.addEventListener('input', evt => {
+            checkInputValidity(formElement, inputItem)
+        })
+    })
+}
+
 
 function enableValidation({formElement, inputElement, buttonElement, errorElement, errorClass, buttonDisableClass}) {
-    const forms = Array.from(document.querySelectorAll(formElement));
-    forms.forEach(form => {
-        // отменяем отправку формы
-        form.addEventListener('submit', function (evt) {
-            evt.preventDefault();
-        });    
-    });
+    // * запуск процесса валидации
+    const formList = Array.from(document.querySelectorAll(formElement));
+    const formListIterator = (formList) => {
+        formList.forEach(form => {
+            const handelFormSubmit = (evt) => {
+                evt.preventDefault();
+            };
+            form.addEventListener('submit', handelFormSubmit);
+            setEventListeners(form, inputElement)
+        });
+    };
+    formListIterator(formList);
 };
 
 enableValidation(obj);
+
