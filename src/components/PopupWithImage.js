6e782bb78fe9f853f1b6c6b0f7a343bd43995c6f
Katya Akilina <katyaartline@gmail.com>
2022-07-20 00:46:17 +0300

    fix: method open() in PopupWithImage

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -4,13 +4,13 @@ export default class PopupWithImage extends Popup {
     constructor(popupSelector) {
         super(popupSelector);
         this._image = this._popupElement.querySelector('.popup__big-foto');
+        this._imageTitle = this._popupElement.querySelector('.popup__title-big-image')
     }
 
     open(item) { // открывает одну карточку
-        // debugger
         this._link = item.link;
         this._name = item.name;
-        this._popupElement.querySelector('.popup__title-big-image').textContent = this._name;
+        this._imageTitle.textContent = this._name;
         this._image.src = this._link;
         this._image.alt = this._name;
         super.open(); // добавляем модификатор '.popup_opend'
